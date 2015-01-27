62dd631b5c2a6a0980d11837402d238ffd00bcee
anurse <andrew@andrewnurse.net>
2015-01-27 10:02:05 -0800

    initial commit

---
+++ git-credential-winstore.ps1
@@ -0,0 +1,8 @@
+Recipe git-credential-winstore {
+    version "1.4-beta"
+    url "http://gitcredentialstore.codeplex.com/downloads/get/834616"
+
+    on install {
+        system git-credential-winstore.exe
+    }
+}
