a5f243a17b4212f975d558f952920789f7becc3e
anurse <andrew@andrewnurse.net>
2015-02-26 15:46:54 -0800

    add hash to git-credential-winstore

--- git-credential-winstore.ps1
+++ git-credential-winstore.ps1
@@ -1,6 +1,6 @@
 Recipe git-credential-winstore {
     version "1.2.0"
-    url "https://github.com/anurse/git-credential-winstore/releases/download/1.2.0/git-credential-winstore.exe"
+    url "https://github.com/anurse/git-credential-winstore/releases/download/1.2.0/git-credential-winstore.exe" -Hash "30A9319D6401A0BAF46C97077F9078090F40CC06CF96496D7156ED965481095E"
 
     on install {
     	# Git doesn't like using cmd files as credential helpers because it uses a POSIX shell to execute commands...
